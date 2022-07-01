09f54d1734ae6239e09e8ca6f6e030414b75fff3
lmnopasdy <sunny.dy@adamson.edu.ph>
2022-07-01 16:19:11 +0800

    Create Merchant.php
    feat:Created Merchant Model

---
+++ app/Models/Merchant.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Merchant extends Model
+{
+    use HasFactory;
+
+    protected $table = 'merchant';
+
+    protected $fillable = [
+        'id','email','password','first_name','last_name','contact_number','name_of_business','about'
+        
+    ];
+}
