4c4eda1272a0af3b7b08285137f7b03b7c63abec
ni-kohl <66339479+ni-kohl@users.noreply.github.com>
2022-07-11 11:15:56 +0800

    refactor: Updated OngoingModal

--- src/components/Orders/components/ModalOngoing.vue
+++ src/components/Orders/components/ModalOngoing.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-
     <b-button @click="layer.show1 = true" id=but1 style="margin-left:50ch;">See Details </b-button>
 
     <popup-layer :visible.sync="layer.show1" @onOpen="open" @onClose="close">
-      <MapLayer />
-      <b-button @click="layer.show1 = false" id=but1 style="margin-left:50ch;">Back</b-button>
+
+      <div>
+         <nav class="navbar navbar-expand-lg navbar-light menu">
+          <div class="container-fluid OrderNumber">
+            <b-button @click="layer.show1 = false" id=but2>Back</b-button>
+            <h3 style="align-items:center;">Order #133443383590</h3>
+            <button variant="light"><span class="fa fa-circle-info circle-info"></span></button>
+          </div>
+        </nav>
+        <MapLayer />
+      </div>
     </popup-layer>
   </div>
 </template>
@@ -15,7 +21,7 @@
 <script>
 import MapLayer from './MapLayer.vue'
 export default {
-  name: "GoogleMap",
+  name: "ModalOngoing",
   data() {
     return {
       layer: {
@@ -35,15 +41,35 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scope>
 #but1 {
   background-color: yellow;
   color: black;
-  font-family: 'Poppins','sans-serif';
+  font-family: 'Poppins', 'sans-serif';
   font-size: 1em;
   padding: .5em;
-  padding-left:1rem;
-  padding-right:1rem;
-  border-radius:4rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  border-radius: 4rem;
+}
+
+#but2 {
+  background-color: yellow;
+  color: black;
+  font-family: 'Poppins', 'sans-serif';
+  font-size: 1em;
+  border-radius: 4rem;
+}
+
+.OrderNumber {
+  background-color: white;
+  color: black;
+  float: center;
+}
+
+.circle-info {
+  color: #e7ba09;
+  width: 2rem;
+  height: 2rem;
 }
 </style>
