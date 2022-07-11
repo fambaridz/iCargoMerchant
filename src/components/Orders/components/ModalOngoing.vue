064986ffc7696b221702a26809113168736b7205
JCCiasico <carlociasico01@gmail.com>
2022-07-11 11:05:32 +0800

    chore: Finished merging the panels

---
+++ src/components/Orders/components/ModalOngoing.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+    <b-button @click="layer.show1 = true" id=but1 style="margin-left:50ch;">See Details </b-button>
+
+    <popup-layer :visible.sync="layer.show1" @onOpen="open" @onClose="close">
+      <MapLayer />
+      <b-button @click="layer.show1 = false" id=but1 style="margin-left:50ch;">Back</b-button>
+    </popup-layer>
+  </div>
+</template>
+
+<script>
+import MapLayer from './MapLayer.vue'
+export default {
+  name: "GoogleMap",
+  data() {
+    return {
+      layer: {
+        show1: false,
+      }
+    };
+  },
+  methods: {
+    open() {
+      console.log("GoogleMap");
+    },
+    /**
+     * @param {Boolean} isPopstate：Whether the close shell event comes from the physical return key
+     */
+  },
+  components: { MapLayer }
+}
+</script>
+
+<style scoped>
+#but1 {
+  background-color: yellow;
+  color: black;
+  font-family: 'Poppins','sans-serif';
+  font-size: 1em;
+  padding: .5em;
+  padding-left:1rem;
+  padding-right:1rem;
+  border-radius:4rem;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,86 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import ongoing from '../components/Orders/components/OngoingOrder.vue'
+import complete from '../components/Orders/components/CompleteOrders.vue'
+import cancel from '../components/Orders/components/CancelledOrders.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import(/* webpackChunkName: "profile" */ '../components/Profile/HomeProfile.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/Help Center/HelpBody.vue')
+  },
+  {
+    name:'signinPage',
+    path: '/',
+    component: () => import(/* webpackChunkName: "signin" */ '../components/Login/SigninPage.vue')
+  },
+  {
+    name:'signupForm',
+    path: '/signup',
+    component: () => import(/* webpackChunkName: "signup" */ '../components/Login/signupForm.vue')
+  },
+  {
+    name:'registration',
+    path: '/registration',
+    component: () => import(/* webpackChunkName: "register" */ '../components/Login/RegistrationSuccess.vue')
+  },
+  {
+    name:'verify',
+    path: '/verify',
+    component: () => import(/* webpackChunkName: "register" */ '../components/Login/AccountVerify.vue')
+  },
+  {
+    name:'price',
+    path: '/price',
+    component: () => import(/* webpackChunkName: "register" */ '../components/New Booking/Price/NewBooking.vue')
+  },
+  {
+    name:'routes',
+    path: '/routes',
+    component: () => import(/* webpackChunkName: "register" */ '../components/New Booking/Routes/ICARGO-HEADER.vue')
+  },
+  {
+    name:'details',
+    path: '/details',
+    component: () => import(/* webpackChunkName: "register" */ '../components/New Booking/Other Details/NavBody.vue')
+  },
+  {
+    name:'toship',
+    path: '/toship',
+    component: () => import(/* webpackChunkName: "register" */ '../components/Orders/components/ToShipOrder.vue')
+  },
+  {
+    name:'ongoing',
+    path: '/ongoing',
+    component: ongoing
+  },
+  {
+    name:'complete',
+    path: '/complete',
+    component: complete
+  },
+  {
+    name:'cancel',
+    path: '/cancel',
+    component: cancel
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
