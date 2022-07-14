6443496cf4aba9cf5ef9c011a0d05285fc09018d
ni-kohl <66339479+ni-kohl@users.noreply.github.com>
2022-07-14 12:10:27 +0800

    refactor: Updated To Ship

--- src/components/Orders/components/ToShipOrder.vue
+++ src/components/Orders/components/ToShipOrder.vue
@@ -14,86 +14,82 @@
                 </div>
             </b-tabs>
         </div>
-        <br>
-        <b-container class="cancelled">
-            <b-row>
-                <div class="container-fluid titleone">
-                    <div class="row">
-                        <div class="col-6 text-left" style="height: 10px;">
-                            <span style="color:white ; font-weight:bold; margin-left:1ch; font-size: large;">March 25,
-                                2022 3:00PM</span>
-                        </div>
-                        <div class="col-15 text-left" style="margin-left:2ch;">
-                            <div class="mb-1">
-                                <b-button id="cancel" @click="showMsgBoxTwo" style="margin-left:50ch;">Cancel Order
-                                </b-button>
-                                {{ String(boxTwo) }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="container-fluid titletwoa">
-                    <i class="fa-regular fa-circle" style="color:#FBCD10; margin-left: 3ch;"></i>
-                    &nbsp;&nbsp;&nbsp;&nbsp;<span id="user">House No., Street, City, Country, Postal Code</span> <br>
-                    <br>
-
-
-                    <i class="fa-solid fa-location-dot" style="color:#FBCD10; margin-left: 3ch;"></i>
-                    &nbsp;&nbsp;&nbsp;&nbsp;<span id="user">House No., Street, City, Country, Postal Code</span>
-                    <!--b-button variant="light" id="cancel" style="margin-left:50ch;">Cancel</b-button-->
-
-                </div>
-
-                <div class="container-fluid titletwob">
-                    <div class="row">
-                        <div class="col-6 text-left" style="color: black; height: 20px;">
-                            <span style="color:black ; font-weight:bold; margin-left: 10ch;">Truck</span>
-                        </div>
-                        <div class="col-6 text-center">
-                            <span style="color:navy; font-weight:bold;">P424</span>
-                        </div>
-                    </div>
-                </div>
-                </b-row>
+        <b-container class="toship">
+            <b-nav>
+                <!--ORDER-->
                 <br>
                 <b-row>
-                <div class="container-fluid titletwo">
-                    <div class="row">
-                        <div class="col-6 text-left" style="height: 10px;">
-                            <span style="color:white ; font-weight:bold; margin-left:1ch; font-size: large;">March 25,
-                                2022 3:00PM</span>
-                        </div>
-                        <div class="col-15 text-left" style="margin-left:2ch;">
-                            <div class="mb-1">
-                                <b-button id="cancel" @click="showMsgBoxTwo" style="margin-left:50ch;">Cancel Order
+                    <div class="container-fluid box-header">
+                        <div class="row">
+                            <div class="col-6 text-left" style="height: 10px;">
+                                <h3 id="date-time">March 25,2022 3:00PM</h3>
+                            </div>
+                            <div class="modal-button">
+                                <b-button id="cancel" @click="showMsgBoxTwo">Cancel Order
                                 </b-button>
                                 {{ String(boxTwo) }}
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="container-fluid titletwoa">
-                    <i class="fa-regular fa-circle" style="color:#FBCD10; margin-left: 3ch;"></i>
-                    &nbsp;&nbsp;&nbsp;&nbsp;<span id="user">House No., Street, City, Country, Postal Code</span> <br>
-                    <br>
+                    <div class="container-fluid address">
+                        <i class="fa-regular fa-circle" style="color:#FBCD10; margin-left: 3ch;"></i>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<span id="pu-address">House No., Street, City, Country, Postal
+                            Code</span> <br>
+                        <br>
+                        <i class="fa-solid fa-location-dot" style="color:#FBCD10; margin-left: 3ch;"></i>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<span id="do-address">House No., Street, City, Country, Postal
+                            Code</span>
+                    </div>
 
-
-                    <i class="fa-solid fa-location-dot" style="color:#FBCD10; margin-left: 3ch;"></i>
-                    &nbsp;&nbsp;&nbsp;&nbsp;<span id="user">House No., Street, City, Country, Postal Code</span>
-                    <!--b-button variant="light" id="cancel" style="margin-left:50ch;">Cancel</b-button-->
-                </div>
-
-                <div class="container-fluid titletwob">
-                    <div class="row">
-                        <div class="col-6 text-left" style="color: black; height: 20px;">
-                            <span style="color:black ; font-weight:bold; margin-left: 10ch;">Motorcycle</span>
-                        </div>
-                        <div class="col-6 text-center">
-                            <span style="color:navy; font-weight:bold;">P123</span>
+                    <div class="container-fluid box-footer">
+                        <div class="row">
+                            <div class="col-6 text-left" style="color: black; height: 20px;">
+                                <span id="vehicle-type">Truck</span>
+                            </div>
+                            <div class="col-6 text-center">
+                                <span id="price">P424</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </b-row>
+                </b-row>
+                <!--ORDER-->
+                <br>
+                <b-row>
+                    <div class="container-fluid box-header">
+                        <div class="row">
+                            <div class="col-6 text-left" style="height: 10px;">
+                                <h3 id="date-time">March 25,2022 3:00PM</h3>
+                            </div>
+                            <div class="modal-button">
+                                <b-button id="cancel" @click="showMsgBoxTwo">Cancel Order
+                                </b-button>
+                                {{ String(boxTwo) }}
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="container-fluid address">
+                        <i class="fa-regular fa-circle" style="color:#FBCD10; margin-left: 3ch;"></i>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<span id="pu-address">House No., Street, City, Country, Postal
+                            Code</span> <br>
+                        <br>
+                        <i class="fa-solid fa-location-dot" style="color:#FBCD10; margin-left: 3ch;"></i>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<span id="do-address">House No., Street, City, Country, Postal
+                            Code</span>
+                    </div>
+
+                    <div class="container-fluid box-footer">
+                        <div class="row">
+                            <div class="col-6 text-left" style="color: black; height: 20px;">
+                                <span id="vehicle-type">Truck</span>
+                            </div>
+                            <div class="col-6 text-center">
+                                <span id="price">P424</span>
+                            </div>
+                        </div>
+                    </div>
+                </b-row>
+            </b-nav>
         </b-container>
     </div>
 </template>
@@ -143,7 +139,7 @@ export default {
     background-color: white;
 }
 
-.titleone {
+.box-header {
     background-color: #003060;
     padding: .4rem;
     color: white;
@@ -151,15 +147,7 @@ export default {
     width: 50rem;
 }
 
-.titletwo {
-    background-color: #003060;
-    padding: .4rem;
-    color: white;
-    padding-left: 2rem;
-    width: 50rem;
-}
-
-.titleonea {
+.address {
     background-color: #ffffff;
     padding: 1rem;
     color: rgb(8, 8, 8);
@@ -168,17 +156,7 @@ export default {
     border: 1px solid rgb(156, 151, 151);
 }
 
-.titletwoa {
-    background-color: #ffffff;
-    padding: 1rem;
-    color: rgb(4, 0, 0);
-    padding-left: 2rem;
-    width: 50rem;
-    border: 1px solid rgb(156, 151, 151);
-
-}
-
-.titleoneb {
+.box-footer {
     background-color: rgb(224, 224, 238);
     padding: 0rem;
     color: rgb(8, 8, 8);
@@ -188,17 +166,27 @@ export default {
     text-align: left;
 }
 
-.titletwob {
-    background-color: rgb(224, 224, 238);
-    padding: 0rem;
-    color: rgb(8, 8, 8);
-    padding-left: 1rem;
-    width: 50rem;
-    border: 1px solid rgb(156, 151, 151);
+
+#date-time {
+    color: white;
+    font-weight: bold;
+    margin-left: 1ch;
+    font-size: large;
+}
+
+#vehicle-type {
+    color: black;
+    font-weight: bold;
+    margin-left: 10ch;
+}
+
+#price {
+    color: navy;
+    font-weight: bold;
 }
 
 #cancel {
-    background-color: yellow;
+    background-color: #FBCD10;
     color: black;
     font-family: 'Poppins', 'sans-serif';
     font-size: 1em;
@@ -206,6 +194,7 @@ export default {
     padding-left: 1rem;
     padding-right: 1rem;
     border-radius: 4rem;
+    margin-left: 50ch;
 }
 
 .topnav {
@@ -239,5 +228,61 @@ export default {
 
 a.disable:hover {
     background-color: #FBCD10;
+}
+
+@media (max-width:700px) {
+
+    .topnav {
+        width: 25em;
+    }
+
+    .box-header {
+        width: 22rem;
+    }
+
+    .address {
+        width: 22rem;
+    }
+
+    .box-footer {
+        width: 22rem;
+    }
+
+    #cancel {
+        margin-left: 14ch;
+        width: 9rem;
+    }
+}
+
+@media (max-width:350px) {
+
+    .toship {
+        width: fit-content;
+    }
+
+    .topnav {
+        width: 19em;
+    }
+
+    .topnav a {
+        width: 25%;
+    }
+
+    .box-header {
+        width: 17rem;
+    }
+
+    .address {
+        width: 17rem;
+    }
+
+    .box-footer {
+        width: 17rem;
+    }
+
+    #cancel {
+        margin-left: 9.5ch;
+        width: 8rem;
+    }
 }
 </style>
