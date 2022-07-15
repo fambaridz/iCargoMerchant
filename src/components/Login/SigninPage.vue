40b5195b54eaa68ab7b7be1b720ff77d2531866b
JCCiasico <carlociasico01@gmail.com>
2022-07-15 14:07:10 +0800

    refactor: updated SigninPage

--- src/components/Login/SigninPage.vue
+++ src/components/Login/SigninPage.vue
@@ -1,57 +1,31 @@
 <template>
-  <div class="background">
-    <form class="custom-form" @submit.prevent="loginUser">
+  <div class="container-fluid pt-5 p-1 background">
+    <form @submit.prevent="loginUser">
       <h1 class="text-dark text-center">
-        Sign in to <span class="text">iCargo</span
-        ><span class="text-warning"> Pacific</span>
+        Sign in to <span class="text">iCargo</span><span class="text-warning"> Pacific</span>
       </h1>
       <label class="text-left text-dark">Email </label>
-      <v-text-field
-        id="email"
-        v-model="auth.email"
-        class="field d-block mx-auto"
-        :rules="[rules.required, rules.email]"
-        name="email"
-        placeholder="Email"
-        rounded
-        solo
-        filled
-      ></v-text-field>
+      <v-text-field id="email" v-model="auth.email" class="field d-block mx-auto" :rules="[rules.required, rules.email]"
+        name="email" placeholder="Email" rounded solo filled></v-text-field>
       <label class="text-left text-dark">Password </label>
 
-      <v-text-field
-        id="password"
-        v-model="auth.password"
-        name="password"
-        class="field d-block mx-auto"
-        placeholder="Password"
-        rounded
-        solo
-        filled
-        :rules="[rules.required]"
-        :type="show ? 'text' : 'password'"
-        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-        @click:append="show = !show"
-      ></v-text-field>
+      <v-text-field id="password" v-model="auth.password" name="password" class="field d-block mx-auto"
+        placeholder="Password" rounded solo filled :rules="[rules.required]" :type="show ? 'text' : 'password'"
+        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" @click:append="show = !show"></v-text-field>
 
       <h5 class="text-right p-0 m-0 text-dark font-weight-normal">
         <a href="#" class="text">Forgot password?ㅤㅤ</a>
       </h5>
       <br />
       <div class="text-center">
-        <a to=" " class="btn btn-light btn-lg shadow text-muted">ㅤBackㅤ</a>
-        <button type="submit" class="btn btn-warning btn-lg shadow text-light"
-        >
+        <a to=" " id ="buttons" class="btn btn-light btn-lg shadow text-muted">ㅤBackㅤ</a>
+        <button type="submit" id="buttons" class="btn btn-warning btn-lg shadow text-light">
           ㅤSIGN INㅤ
         </button>
       </div>
-      <h5 class="p-4 text-dark">
-        Don’t have an account yet?<a
-          class="font-weight-normal text"
-          href="/signup"
-        >
-          Sign up now.</a
-        >
+      <h5 class="m-4 text-dark">
+        Don’t have an account yet?<a class="font-weight-normal text" href="/signup">
+          Sign up now.</a>
       </h5>
     </form>
   </div>
@@ -84,31 +58,31 @@ export default {
 
   methods: {
     async loginUser() {
- 
+
 
       axios.defaults.baseURL = "http://127.0.0.1:8000";
-    await axios.get("/sanctum/csrf-cookie")
-  
+      await axios.get("/sanctum/csrf-cookie")
+
       await axios
         .post("/api/merchantlogin", this.auth)
-        
+
         .then((response) => {
-             localStorage.setItem("token", response.data.token); 
-       //   this.$router.push({name:'details'});
-          
-              this.$router.push('/details', () => this.$router.go(0)).catch(err => {})
+          localStorage.setItem("token", response.data.token);
+          //   this.$router.push({name:'details'});
 
-          
-  
+          this.$router.push('/details', () => this.$router.go(0)).catch(err => { })
 
-    
- 
-       
+
+
+
+
+
+
         })
         .catch((err) => {
           //  console.log(err)
         })
-      
+
     },
 
     /*    goToHome() {
@@ -119,33 +93,40 @@ export default {
 </script>
 
 <style scoped>
-.field {
+.field{
   width: 600px;
   margin: auto;
 }
 
-.text {
+.text{
   color: #3eb3f4;
 }
 
-.background {
-  background: linear-gradient(
-    to bottom right,
-    #280eb8,
-    #3352db,
-    #4c9cca,
-    rgb(79, 135, 187)
-  );
+
+div.background{
+  background: linear-gradient(to bottom right, #280eb8, #3352db, #4c9cca, rgb(79, 135, 187));
+  padding:0;
+  margin: 0;
   height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+}
+
+/*Forgot password?*/
+h5.text-right{
+  text-align:right;
+  margin-right: 15%;
+}
+/*Sign up*/
+h5.p-4{
+  margin-left: 20px;
+  margin-top: 20px;
 }
 
 h1 {
-  font-weight: bold;
+  font-weight:bold;
   padding-bottom: 40px;
+
 }
+
 h5 {
   margin-top: 20px;
   margin-bottom: 10px;
@@ -153,39 +134,83 @@ h5 {
   font-weight: normal;
 }
 
-h5.text-right {
-  text-align: right;
-  margin-right: 15%;
-  margin-bottom: 50px;
+#buttons {
+  margin: 10px 15px;
+  padding: 6px 6px;
+  border-radius: 18px;
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 1.4em;
 }
 
-h5.p-4 {
-  margin-left: 20px;
-  margin-top: 40px;
-}
-
-a,
-button {
+a {
   margin: 0px auto;
-  border-radius: 50px;
   text-decoration: none;
 }
 
-form {
+a:hover{
+  color: #ffc107;
+}
+
+form{
   font-family: Roboto;
   max-width: 760px;
+  margin: 0px auto;
+  margin-bottom: 100px;
   background: white;
   text-align: left;
   padding: 40px 40px 10px 40px;
-  border-radius: 25px;
+  border-radius: 50px;
 }
 
-label {
+label{
   color: #aaa;
   display: inline-block;
   margin: 5px 0px 5px 50px;
   font-size: 1em;
   letter-spacing: 1px;
   font-weight: normal;
+}
+
+/*responsiveness*/
+@media(max-width:600px){
+  #buttons{
+    font-size: 1.3rem;
+    margin: 20px 10px;
+  }
+  h5.p-4{
+    margin-top: 15px;
+  }
+  label{
+    margin: 5px 0px 5px 20px;
+  }
+}
+
+@media(max-width:375px){
+  #buttons{
+    margin: 10px 10px;
+    font-size:1.1rem;
+    border-radius: 15px;
+  }
+  h5{
+    font-size: 1.1rem;
+  }
+}
+@media(max-width:320px){
+  #buttons{
+    margin: 10px 5px;
+    font-size:1rem;
+    border-radius: 15px;
+  }
+  h5.p-4{
+    text-align: center;
+  }
+}
+@media(max-width:280px){
+  #buttons{
+    margin: 10px 1px;
+    font-size:1rem;
+    border-radius: 15px;
+  }
 }
 </style>
