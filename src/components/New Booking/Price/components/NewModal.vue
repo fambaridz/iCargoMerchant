6dc9ffc562d184d0478a9a090edce25bf98aa6ae
christinebrito <108043669+christinebrito@users.noreply.github.com>
2022-07-22 18:27:25 +0800

    remove inclusions

--- src/components/New Booking/Price/components/NewModal.vue
+++ src/components/New Booking/Price/components/NewModal.vue
@@ -2,8 +2,8 @@
   <div id="modal">
    <div><br></div>
    <center>
-     <a href="/details" class="button show-btn1">BACK</a>&nbsp;&nbsp;
-    <b-button id="show-btn" @click="showModal">PLACE ORDER </b-button>
+     <a href="/details" class="button show-btn1">Back</a>&nbsp;&nbsp;
+    <b-button id=show-btn @click="showModal">Place Order </b-button>
     </center>
     <b-modal ref="my-modal" title="YOUR ECONOMY PRICE" hide-footer>
       <div class="d-block">
@@ -16,7 +16,7 @@
               <span style="color:black ; ">Destination :</span><br>
               <span style="color:black ;  ">Distance    : </span><br>
               <span style="color:black ; ">Duration    : </span><br>
-              <span style="color:black ;  ">Inclusions  : </span><br>
+              <!-- <span style="color:black ;  ">Inclusions  : </span><br> -->
               <span style="color:black ;  ">Available   : </span>
             </div>
             <div class="col-6">
@@ -25,11 +25,13 @@
               <span style="color:grey;"> Calatagan, Batangas </span><br>
               <span style="color:grey; "> 86 km </span><br>
               <span style="color:grey; "> 4h 45m </span><br>
-              <span style="color:grey; "> Driver,fuel,load,/unload,insurance </span><br>
+              <!-- <span style="color:grey; "> Driver,fuel,load,/unload,insurance </span><br> -->
               <span style="color:grey; "> Earliest pickup within an hour.Later pickup times are also available.  </span>
             </div>
             <br>
+            <br>
             <h6> Your driver must receive your advance approval<br>to reimburse tolls and parking.</h6>
+            <br>
             <br>
     </div>
     <pre class="text-center"><b-button pill @click="hideModal()" variant="secondary">    CLOSE    </b-button>  <b-button pill href="/toship" variant="primary">   BOOK NOW   </b-button></pre>
@@ -65,33 +67,29 @@ h6{
 
 #show-btn{
   background-color: #FBCD10;
-  color: white;
+  color: black;
   font-family: 'Poppins','sans-serif';
-  font-size: 20px;
-  margin-left: 15px; /* spacing in between buttons */
+  font-size: 1em;
   padding: .5rem;
   padding-left:1rem;
   padding-right:1rem;
   border-radius:4rem;
-  box-shadow: 0px 2px 2px 1px #aaa;
+  box-shadow: 0px 3px 10px 1px gray;
  
 
 }
-
 .show-btn1{
-  background-color: #FBCD10;
-  width: 125px;
-  color: white;
+   background-color: #FBCD10;
+  color: black;
   font-family: 'Poppins','sans-serif';
-  font-size: 20px;
-  margin-right: 15px; /* spacing in between buttons */
+  font-size: 1em;
   padding: .5rem;
-  padding-left:2.4rem;
-  padding-right:2.4rem;
+  padding-left:2rem;
+  padding-right:2rem;
   border-radius:4rem;
   border: .5px solid grey;
   text-decoration:none;
-  box-shadow: 0px 2px 2px 1px #aaa;
+  box-shadow: 0px 3px 10px 1px gray;
 }
 #modal{
   padding-bottom: 50px;
